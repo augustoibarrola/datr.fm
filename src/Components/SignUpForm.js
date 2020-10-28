d48063c09b2a5ccaa715ef72b0a6510b64958e6a
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-10-28 14:03:08 -0400

    first commit; test commit to make sure git repo is set up correctly

---
+++ src/Components/SignUpForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Signup() {
+    return(  
+        <div> 
+            <h1>Sign Up</h1>
+            <form>
+                <input placeholder="test"/> 
+            </form>
+        </div>
+    )
+}
+
+export default Signup
