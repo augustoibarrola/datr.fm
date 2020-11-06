b36130428542ae0455096537badca4c75bebab8b
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-11-05 19:42:02 -0500

    styling to ne messages component

--- src/Components/NewMessage.js
+++ src/Components/NewMessage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Form, FormControl, Button, InputGroup } from 'react-bootstrap'
 
 function NewMessage(props) {
     // console.log("NEWMESSAGE_PROPS =>", props)
@@ -10,38 +11,49 @@ function NewMessage(props) {
   
     const usersArray = props.users.filter( user => user.id !== props.user.id)
 
+    const formContainerStyling = {
+        maxWidth: '60%'
+    }
+
+    const buttonStyling = {
+        marginTop: '15px'
+    }
+
 
     return(
         <div> 
-            <h2>New Message</h2> 
-            <form onSubmit={(event) => {
+
+
+        <div>
+
+            <Form onSubmit={(event) => {
                 props.messagesSubmitHandler(event, recipient)
                 setRecipient('')
                 setMsgBody('')
-                }}>
-                <div>
-                    <label >From: </label>
-                    <input type="text" value={sender.name} onChange={(event) => setSender(event.target.value)} disabled />
-                </div>
+                }} styling={formContainerStyling}>
 
-                <div>
-                    <label >To: </label>
-                    <select value={recipient} onChange={(event) => setRecipient(event.target.value)}> 
-                        {usersArray.map(user => <option value={user.name} id={user.id}> {user.name} </option>)} */}
-                    {/*  */}
-                    </select>
-                </div>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label> From </Form.Label>
+                    <Form.Control type="email" placeholder={sender.name} readOnly />
+                </Form.Group>
 
-                <div>
-                    <label >Message: </label>
-                    <input type="textarea" value={msgBody} onChange={(event) => setMsgBody(event.target.value)}/>
-                </div>
+                <Form.Group controlId="exampleForum.SelectCustom">
+                    <Form.Label> To </Form.Label> 
+                    <Form.Control as="select" value={recipient} onChange={(event) => setRecipient(event.target.value)} custom >
+                    {usersArray.map(user => <option value={user.name} id={user.id}> {user.name} </option>)} 
+                    </Form.Control>
+                </Form.Group>
 
-                <div>
-                    <input type="submit"/>
-                </div>
+                <InputGroup>
+                    <FormControl as="textarea" aria-label="With textarea" value={msgBody} onChange={(event) => setMsgBody(event.target.value)}/>
+                </InputGroup>
 
-            </form>
+
+                <Button variant="primary" type="submit" style={buttonStyling}>
+                    Send Message
+                </Button>
+            </Form >
+        </div>
         </div>
     )
 }
@@ -50,3 +62,37 @@ export default NewMessage
 
 
 
+
+{/* <div> 
+<h2>New Message</h2> 
+
+
+<form onSubmit={(event) => {
+    props.messagesSubmitHandler(event, recipient)
+    setRecipient('')
+    setMsgBody('')
+    }}>
+
+    <div>
+        <label >From: </label>
+        <input type="text" value={sender.name} onChange={(event) => setSender(event.target.value)} disabled />
+    </div>
+
+    <div>
+        <label >To: </label>
+        <select value={recipient} onChange={(event) => setRecipient(event.target.value)}> 
+            {usersArray.map(user => <option value={user.name} id={user.id}> {user.name} </option>)} 
+        </select>
+    </div>
+
+    <div>
+        <label >Message: </label>
+        <input type="textarea" value={msgBody} onChange={(event) => setMsgBody(event.target.value)}/>
+    </div>
+
+    <div>
+        <input type="submit"/>
+    </div>
+
+</form>
+</div> */}
