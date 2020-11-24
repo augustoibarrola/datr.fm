b37866284abe68d7519bbc659651139ba19664db
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-11-23 22:16:54 -0500

    changed signupform component from function to const

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -2,46 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Form, InputGroup, FormControl, Button } from 'react-bootstrap'
 
-function SignUpForm(props) {
-
-    // return(  
-    //     <div> 
-    //         <h1>Sign Up</h1>
-    //         <form onSubmit={(event) => props.signUpSubmitHandler(event)}>
-                
-    //             <div>
-    //                 <label>Name:</label>
-    //                 <input type="text"/>
-    //             </div>
-
-    //             <div>
-    //                 <label>UserName:</label>
-    //                 <input type="text"/>
-    //             </div>
-
-    //             <div>
-    //                 <label>Email: </label>
-    //                 <input type="email" />
-    //             </div>
-
-    //             <div>
-    //                 <label>Password:</label>
-    //                 <input type="password"/>
-    //             </div>
-
-    //             <div>
-    //                 {/* <Redirect to="/" > */}
-    //                     <input type="submit"/>
-    //                 {/* </Redirect> */}
-    //             </div>
-
-    //         </form>
-
-    //         <Link to="/lastfm"> Sign In With Last.fm </Link>
-    //           {/* make above the sign in button to last.fm sign in  */}
-
-    //     </div>
-    // )
+const SignUpForm = (props) => {
 
     const signUpHeadingStyling = {
         paddingBottom: '15px'
